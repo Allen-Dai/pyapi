b18dd5715bd9910704e8317dd3e9ddfbe6460e7f
Allen Day <allenday0114@gmail.com>
2021-04-26 19:22:30 -0400

    Get/Put request added

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-from flask import Flask
-from flask_restful import Resource
-import pymongo, loadenv
+from flask import Flask, request
+from flask_restful import Api, Resource 
+import pymongo, loadenv, json
 
 client = pymongo.MongoClient(loadenv._get("DB_URL"))
+project = loadenv._get("PROJECT")
 
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+
+
+class Bug(Resource):
+        
+    def get(self,user):
+        cursor = client[project]["bug"].find({"user":user},{"_id": False})
+        bugs = []
+        for bug in cursor:
+            bugs.append(bug)
+        return bugs
+
+    def put(self, user):
+        try:
+            bug_id = int(request.form["bug_id"])
+            update_content = json.loads(request.form["update"])
+
+            
+            client[project]["bug"].update_one({"bug_id" : bug_id},{"$set" : update_content}) 
+        except:
+            return {"message":"Errors"}
+
+        
+
+    
+        
+
+api.add_resource(Bug, "/bug/<int:user>")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
 
