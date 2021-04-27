50f4272368a5e5cd0e9d473a9abe9ccb99b7e512
Allen Day <allenday0114@gmail.com>
2021-04-26 20:43:35 -0400

    Create/delete added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
-from flask_restful import Api, Resource 
-import pymongo, loadenv, json
+from flask_restful import Api, Resource, reqparse
+import pymongo, json, string, random
+import loadenv
 
 client = pymongo.MongoClient(loadenv._get("DB_URL"))
 project = loadenv._get("PROJECT")
@@ -12,32 +13,47 @@ api = Api(app)
 
 
 
-
 class Bug(Resource):
         
-    def get(self,user):
-        cursor = client[project]["bug"].find({"user":user},{"_id": False})
+    def get(self,user_id):
+        cursor = client[project]["bug"].find({"user_id":user_id},{"_id": False})
         bugs = []
         for bug in cursor:
             bugs.append(bug)
+
+        if bugs == []:
+            return 204
         return bugs
 
-    def put(self, user):
+    def put(self, user_id):
         try:
-            bug_id = int(request.form["bug_id"])
+            bug_id = request.form["bug_id"]
             update_content = json.loads(request.form["update"])
 
             
             client[project]["bug"].update_one({"bug_id" : bug_id},{"$set" : update_content}) 
         except:
-            return {"message":"Errors"}
+            return {"message":"Errors"}, 404
 
-        
+    def post(self, user_id):
+        data = json.loads(request.form["create"])
+        data["bug_id"] = (''.join(random.choice(string.ascii_letters + string.digits) for _ in range(40)))
+        client[project]["bug"].insert_one(data)
+        return {"message":"succeed"}, 200
+
+
+class DeleteBug(Resource):
+    def delete(self, bug_id):
+        client[project]["bug"].delete_one({"bug_id":bug_id})
+        return {"message":"succeed"}, 200
+
 
     
         
 
-api.add_resource(Bug, "/bug/<int:user>")
+api.add_resource(Bug, "/bug/<string:user_id>")
+
+api.add_resource(DeleteBug, "/bug/delete/<string:bug_id>")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
