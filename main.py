3fe48cbf25fad1d15bf53448b26e4e697443e7d8
Allen Day <allenday0114@gmail.com>
2021-06-20 18:06:24 -0400

    typo

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ async def create_bug(bug: Bug):
 
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
